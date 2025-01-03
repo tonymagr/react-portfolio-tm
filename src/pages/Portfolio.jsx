d564fa55004847c19a0f5d53c8a30f3f5b63ed4f
Tony Magrady <tonymagrady@gmail.com>
2025-01-02 17:02:32 -0800

    in portfolio page forced images/apps 5 thru 8 to 2nd row for better formatting (in index.css and Portfolio.jsx). alternated image frame colors for effect

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,7 +6,7 @@ export default function Portfolio() {
   return (
     <div>
       <div className="h3-portfolio">Portfolio</div>
-      <div className="container pt-0 flex-row">
+      <div className="container pt-0 flex-row portfolio-grid">
         <Project
           links={[
             <Link key={1} className="nav-link text-dark" target="_blank"
@@ -56,7 +56,7 @@ export default function Portfolio() {
             <Link key={1} className="nav-link text-dark" target="_blank"
               to="https://full-stack-service-app.onrender.com/">
               <div style={{ textAlign: 'center' }}>
-                <img className="image-border-lightgreen" src="\home_installation.jpg" alt="Skill Magnet" style={{ width: '130px', marginLeft: '0px', marginTop: '-4px' }} />
+                <img className="image-border-lightblue" src="\home_installation.jpg" alt="Skill Magnet" style={{ width: '130px', marginLeft: '0px', marginTop: '-4px' }} />
                 <div>Skill Magnet</div>
               </div>
             </Link>,
@@ -67,7 +67,7 @@ export default function Portfolio() {
             <Link key={1} className="nav-link text-dark" target="_blank"
               to="https://tonymagr.github.io/weather-dashboard-search/">
               <div style={{ textAlign: 'center' }}>
-                <img className="image-border-lightblue" src="\weather.jpg" alt="Weather Dashboard" style={{ width: '110px', marginLeft: '0px', marginTop: '-4px' }} />
+                <img className="image-border-lightgreen" src="\weather.jpg" alt="Weather Dashboard" style={{ width: '110px', marginLeft: '0px', marginTop: '-4px' }} />
                 <div>Weather Dashboard</div>
               </div>
             </Link>,
@@ -78,7 +78,7 @@ export default function Portfolio() {
             <Link key={1} className="nav-link text-dark" target="_blank"
               to="https://tonymagr.github.io/work-day-scheduler-3rd-party-apis/">
               <div style={{ textAlign: 'center' }}>
-                <img className="image-border-lightgreen" src="\daily_planner.jpg" alt="Work Day Scheduler" style={{ width: '100px', marginLeft: '0px', marginTop: '-4px' }} />
+                <img className="image-border-lightblue" src="\daily_planner.jpg" alt="Work Day Scheduler" style={{ width: '100px', marginLeft: '0px', marginTop: '-4px' }} />
                 <div>Work Day Scheduler</div>
               </div>
             </Link>,
@@ -89,7 +89,7 @@ export default function Portfolio() {
             <Link key={1} className="nav-link text-dark" target="_blank"
               to="https://tonymagr.github.io/quiz-game/">
               <div style={{ textAlign: 'center' }}>
-                <img className="image-border-lightblue" src="\web_app_dev.jpg" alt="Quiz Game" style={{ width: '150px', marginLeft: '0px', marginTop: '-4px' }} />
+                <img className="image-border-lightgreen" src="\web_app_dev.jpg" alt="Quiz Game" style={{ width: '150px', marginLeft: '0px', marginTop: '-4px' }} />
                 <div>Quiz Game</div>
               </div>
             </Link>,
